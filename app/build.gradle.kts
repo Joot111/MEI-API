57f30c801f47e633473e1b9bb111671dcceb2c6e
Joot111 <joaoamoroso04@gmail.com>
2026-05-05 18:42:12 +0100

    adição dependencias retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,12 @@ android {
 }
 
 dependencies {
+    // Add Retrofit Dependencies
+    // https://square.github.io/retrofit/
+    //implementation("com.squareup.retrofit2:retrofit:2.6.2")
+    //implementation("com.squareup.retrofit2:converter-gson:2.3.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
